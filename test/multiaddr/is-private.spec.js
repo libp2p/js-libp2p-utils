b47bf6f7da448385cadb2ecb61e3982e7bc1144b
Vasco Santos <vasco.santos@moxy.studio>
2020-10-07 14:40:32 +0200

    chore: add docs and more tests

--- test/multiaddr/is-private.spec.js
+++ test/multiaddr/is-private.spec.js
@@ -8,44 +8,51 @@ const isPrivate = require('../../src/multiaddr/is-private')
 
 describe('multiaddr isPrivate', () => {
   it('identifies private ip4 multiaddrs', () => {
-    const m1 = multiaddr('/ip4/127.0.0.1/tcp/1000')
-    const m2 = multiaddr('/ip4/10.0.0.1/tcp/1000')
-    const m3 = multiaddr('/ip4/192.168.0.1/tcp/1000')
-    const m4 = multiaddr('/ip4/172.16.0.1/tcp/1000')
-
-    expect(isPrivate(m1)).to.eql(true)
-    expect(isPrivate(m2)).to.eql(true)
-    expect(isPrivate(m3)).to.eql(true)
-    expect(isPrivate(m4)).to.eql(true)
+    [
+      multiaddr('/ip4/127.0.0.1/tcp/1000'),
+      multiaddr('/ip4/10.0.0.1/tcp/1000'),
+      multiaddr('/ip4/192.168.0.1/tcp/1000'),
+      multiaddr('/ip4/172.16.0.1/tcp/1000')
+    ].forEach(ma => {
+      expect(isPrivate(ma)).to.eql(true)
+    })
   })
 
   it('identifies public ip4 multiaddrs', () => {
-    const m1 = multiaddr('/ip4/101.0.26.90/tcp/1000')
-    const m2 = multiaddr('/ip4/40.1.20.9/tcp/1000')
-    const m3 = multiaddr('/ip4/92.168.0.1/tcp/1000')
-    const m4 = multiaddr('/ip4/2.16.0.1/tcp/1000')
-
-    expect(isPrivate(m1)).to.eql(false)
-    expect(isPrivate(m2)).to.eql(false)
-    expect(isPrivate(m3)).to.eql(false)
-    expect(isPrivate(m4)).to.eql(false)
+    [
+      multiaddr('/ip4/101.0.26.90/tcp/1000'),
+      multiaddr('/ip4/40.1.20.9/tcp/1000'),
+      multiaddr('/ip4/92.168.0.1/tcp/1000'),
+      multiaddr('/ip4/2.16.0.1/tcp/1000')
+    ].forEach(ma => {
+      expect(isPrivate(ma)).to.eql(false)
+    })
   })
 
   it('identifies private ip6 multiaddrs', () => {
-    const m1 = multiaddr('/ip6/fd52:8342:fc46:6c91:3ac9:86ff:fe31:7095/tcp/1000')
-
-    expect(isPrivate(m1)).to.eql(true)
+    [
+      multiaddr('/ip6/fd52:8342:fc46:6c91:3ac9:86ff:fe31:7095/tcp/1000'),
+      multiaddr('/ip6/fd52:8342:fc46:6c91:3ac9:86ff:fe31:1/tcp/1000')
+    ].forEach(ma => {
+      expect(isPrivate(ma)).to.eql(true)
+    })
   })
 
   it('identifies public ip6 multiaddrs', () => {
-    const m1 = multiaddr('/ip6/2001:8a0:7ac5:4201:3ac9:86ff:fe31:7095/tcp/1000')
-
-    expect(isPrivate(m1)).to.eql(false)
+    [
+      multiaddr('/ip6/2001:8a0:7ac5:4201:3ac9:86ff:fe31:7095/tcp/1000'),
+      multiaddr('/ip6/2000:8a0:7ac5:4201:3ac9:86ff:fe31:7095/tcp/1000')
+    ].forEach(ma => {
+      expect(isPrivate(ma)).to.eql(false)
+    })
   })
 
-  it('identifies dns multiaddrs as not private', () => {
-    const m1 = multiaddr('/dns4/wss0.bootstrap.libp2p.io/tcp/443')
-
-    expect(isPrivate(m1)).to.eql(false)
+  it('identifies  other multiaddrs as not private addresses', () => {
+    [
+      multiaddr('/dns4/wss0.bootstrap.libp2p.io/tcp/443'),
+      multiaddr('/dns6/wss0.bootstrap.libp2p.io/tcp/443')
+    ].forEach(ma => {
+      expect(isPrivate(ma)).to.eql(false)
+    })
   })
 })
